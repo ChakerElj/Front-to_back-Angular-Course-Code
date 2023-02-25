8a4f3978666cbe5c68ad8a8b24682fde4069cb35
chaker elj <eljchaker232@gmail.com>
2023-02-25 15:41:47 +0100

    proprieties and methods in Angular

--- angularapp/src/app/components/user/user.component.ts
+++ angularapp/src/app/components/user/user.component.ts
@@ -3,10 +3,24 @@ import {Component} from "@angular/core";
 
 @Component({
   selector : 'app-user',
-  template : '<h2>Chaker Elj</h2>'
+
+  templateUrl : './user.component.html',
+  styleUrls : ['./user.component.css']
 })
 
 
 export class UserComponent {
+  // Properties
+  firstName = "Jhon" ;
+  lastName = "Doe" ;
+  age = 30 ;
 
+  // Methods
+  constructor() {
+    this.sayHello();
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`) ;
+  }
 }
